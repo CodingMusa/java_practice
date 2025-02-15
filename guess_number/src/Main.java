c49c5c5434f3b459eea4039985ccb1d1ce76d862
gangdev <dlvlfdn567@gmail.com>
2025-02-15 22:25:23 +0900

    guess_number 추가기능 구현

--- guess_number/src/Main.java
+++ guess_number/src/Main.java
@@ -4,26 +4,47 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Random random = new Random();
-        int randomNum = random.nextInt(100);
         Scanner scanner = new Scanner(System.in);
-        int tryNum = 0;
+        int RandomRangeNum = 0;
+        int chanceNum = 0;
+        int coefficient = 0;
+        int score = 0;
+        int tryCount = 0;
 
-        while (true) {
-            tryNum++;
-            System.out.println("숫자를 입력해주세요: ");
+        System.out.print("난이도를 입력하세요(쉬움, 보통, 어려움): ");
+        String difficultyLevel = scanner.nextLine();
+        if(difficultyLevel.equals("쉬움")){
+            RandomRangeNum = 50;
+            chanceNum = 20;
+            coefficient = 1;
+        } else if(difficultyLevel.equals("보통")) {
+            RandomRangeNum = 100;
+            chanceNum = 15;
+            coefficient = 2;
+        } else {
+            RandomRangeNum = 200;
+            chanceNum = 10;
+            coefficient = 3;
+        }
+
+        int randomNum = random.nextInt(RandomRangeNum);
+
+        while (tryCount <= chanceNum ) {
+            tryCount++;
+
+            System.out.printf("숫자를 입력해주세요: ");
             String str = scanner.nextLine();
             int userNum = Integer.parseInt(str);
 
             if(userNum < randomNum) {
-                System.out.println("입력한 숫자보다 큽니다.");
+                System.out.printf("입력한 숫자보다 큽니다. 남은 횟수: %d\n", chanceNum - tryCount);
             } else if(userNum > randomNum) {
-                System.out.println("입력한 숫자보다 작습니다.");
+                System.out.printf("입력한 숫자보다 작습니다. 남은 횟수: %d\n", chanceNum - tryCount);
             } else {
-                System.out.printf("맞추셨습니다. 시도 횟수: %d", tryNum);
+                score = tryCount * 100 * coefficient;
+                System.out.printf("맞추셨습니다. 시도 횟수: %d/%d, 점수: %d", tryCount, chanceNum, score);
                 break;
             }
         }
-
-
     }
 }
